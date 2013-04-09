e8ff25c329364c42cf1b1bc41c6fa9a3b847868d
Mostafa Sameh <msameh@cloud9ers.com>
2013-04-09 13:51:45 +0200

    1 channel per user instead of one channel per room

--- app/models/ChatRoom.scala
+++ app/models/ChatRoom.scala
@@ -41,59 +41,62 @@ class Room(val name: String) {
   private[this] implicit val timeout = Timeout(1 second)
 
   def join(username: String): Future[(Iteratee[JsValue, _], Enumerator[JsValue])] = {
-    (roomActor ? Join(username)).map {
-      case Connected(enumerator) =>
+	lazy val (userEnumerator, userChannel) = Concurrent.broadcast[JsValue]
+    (roomActor ? Join(username, userChannel)).map {
+      case Connected() =>
         // Create an Iteratee to consume the feed
         val iteratee = Iteratee.foreach[JsValue] { event =>
           roomActor ! Talk(username, (event \ "text").as[String])
         }.mapDone { _ =>
           roomActor ! Quit(username)
         }
-        (iteratee, enumerator) //return value
+        (iteratee, userEnumerator) //return value
 
       case CannotConnect(error) =>
         // Connection error
         // A finished Iteratee sending EOF
         val iteratee = Done[JsValue, Unit]((), Input.EOF)
         // Send an error and close the socket
-        val enumerator = Enumerator[JsValue](JsObject(Seq("error" -> JsString(error)))).andThen(Enumerator.enumInput(Input.EOF))
-        (iteratee, enumerator) //return value
+        val errorEnumerator = Enumerator[JsValue](JsObject(Seq("error" -> JsString(error)))).andThen(Enumerator.enumInput(Input.EOF))
+        (iteratee, errorEnumerator) //return value
     }
   }
-  def members(): Future[Set[String]] = 
+  def members(): Future[Set[String]] =
     (roomActor ? GetMembers()).map {
-    case Members(members) => members
-  }
+      case Members(members) => members
+    }
 }
 
 object Lobby {
   import scala.collection.mutable
   private val roomsMap: mutable.Map[String, Room] = mutable.Map.empty
-  
+
   def rooms(): Seq[String] = roomsMap.keys.toSeq
   def createRoom(name: String): Room =
     synchronized {
-    	if (roomsMap.contains(name))
-    		roomsMap(name)
-		else {
-			val r = new Room(name)
-			roomsMap.put(name, r)
-			r
-		}
+      if (roomsMap.contains(name))
+        roomsMap(name)
+      else {
+        val r = new Room(name)
+        roomsMap.put(name, r)
+        r
+      }
     }
 }
 
 class RoomActor extends Actor with ActorLogging {
   var members: Set[String] = Set.empty
-  lazy val (chatEnumerator, chatChannel) = Concurrent.broadcast[JsValue]
+  import scala.collection.mutable.{ Map => MutableMap }
+  val usersChannels: MutableMap[String, Concurrent.Channel[JsValue]] = MutableMap.empty
 
   def receive = {
-    case Join(username) => {
+    case Join(username, userChannel) => {
       if (members.contains(username)) {
         sender ! CannotConnect("This username is already used")
       } else {
         members += username
-        sender ! Connected(chatEnumerator)
+        usersChannels put (username, userChannel)
+        sender ! Connected()
         self ! NotifyJoin(username)
       }
     }
@@ -110,7 +113,7 @@ class RoomActor extends Actor with ActorLogging {
       members = members - username
       notifyAll("quit", username, "has left the room")
     }
-    case GetMembers() => 
+    case GetMembers() =>
       sender ! Members(members)
   }
   private[this] def notifyAll(kind: String, user: String, text: String) {
@@ -120,17 +123,16 @@ class RoomActor extends Actor with ActorLogging {
         "user" -> JsString(user),
         "message" -> JsString(text),
         "members" -> JsArray(
-          members.toList.map(JsString))
-        ))
-    chatChannel.push(msg)
-  }  
+          members.toList.map(JsString))))
+    usersChannels foreach { case (k, channel) => channel push msg }
+  }
 }
 
-case class Join(username: String)
+case class Join(username: String, userChannel: Concurrent.Channel[JsValue])
 case class Quit(username: String)
 case class Talk(source: String, text: String)
 case class NotifyJoin(username: String)
 case class GetMembers()
 case class Members(members: Set[String])
-case class Connected(enumerator: Enumerator[JsValue])
+case class Connected()
 case class CannotConnect(msg: String)
